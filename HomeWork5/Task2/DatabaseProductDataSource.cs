37224ec5147b52a2c618afc026db41d58c5f0f63
miladshadlou <milad.shadlou@yahoo.com>
2026-09-23 22:25:05 +0330

    impelementation of homework 5

---
+++ HomeWork5/Task2/DatabaseProductDataSource.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork5.Task2
+{
+    public class DatabaseProductDataSource : IProductDataSource
+    {
+        public void GetProducts()
+        {
+            Console.WriteLine("Get Data From Database");
+        }
+    }
+}
---
+++ HomeWork5/Task2/ProductDisplay.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeWork5.Task2
+{
+    public class ProductDisplay
+    {
+        private IProductDataSource _dataSource;
+
+        public ProductDisplay(IProductDataSource dataSource)
+        {
+            _dataSource = dataSource;
+        }
+
+        public void DisplayProducts()
+        {
+            _dataSource.GetProducts();
+        }
+    }
+}
